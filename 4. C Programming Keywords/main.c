59c62b071bacf38dc6fb754f03aaae8bb941ac7c
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-01-18 20:37:30 +0600

    C Programming Keywords

---
+++ 4. C Programming Keywords/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+//C Programming Keywords
+Data Types: int, char, float, double, void, short, long, signed, unsigned
+
+Control Flow: if, else, switch, case, default, for, while, do, break, continue, goto, return
+
+Data Structures: struct, enum, union
+
+Storage Classes: auto, extern, static, register
+
+Others: const, sizeof, volatile
