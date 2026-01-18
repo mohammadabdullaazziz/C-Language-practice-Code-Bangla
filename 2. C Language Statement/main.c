857b77d805ba14e3e74cdc55ac6f8010b7e5ffca
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-01-18 20:10:03 +0600

    C Language Statement

---
+++ 2. C Language Statement/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    // 1. variable statement
+    int age = 20;
+
+    // 2. assignment statement
+    a = 10;
+    b = 20;
+
+    //3. Function Call Statement
+    printf("Hello world!\n");
+
+    // 4. Expression Statement
+    sum = a + b;
+
+    //5. Conditional Statement
+    if (age > 18) {
+    printf("Adult");
+}
+    //6. return statement
+    return 0;
+}
