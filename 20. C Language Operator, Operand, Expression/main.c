e77216eae38f39640fcbe630f90ad9833d09f535
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-02-15 21:56:22 +0600

    C Language Operator, Operand, Expression

---
+++ 20. C Language Operator, Operand, Expression/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  
+  return 0;
+}
+
+// operator
++ , -, *, /, %, ..................
+
+//Operand
+5 + 3 --------5 and 3 operand
+2 + 2 --------2 and 2 operand
+
+//Expression
+5 + 3 
+2 + 2 
+
+//statement
+
+int myNum = 100 + 50;
