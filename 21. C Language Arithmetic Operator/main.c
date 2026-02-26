037589e0907cb0ed509eaa60245b2c10614065e0
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-02-26 21:26:56 +0600

    Refactor main.c to include average calculation

--- 21. C Language Arithmetic Operator/main.c
+++ 21. C Language Arithmetic Operator/main.c
@@ -28,3 +28,30 @@ int main()
   return 0;
 }
 
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, sum;
+    double average; 
+
+    printf("Enter first number : ");
+    scanf("%d", &num1); 
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+
+   //(Type Casting)
+    average = (double)sum / 2;
+
+    printf("\n--- Result ---\n");
+    printf("Sum: %d + %d = %d\n", num1, num2, sum);
+    printf("Average: %.2lf\n", average);
+
+    return 0;
+}
+
