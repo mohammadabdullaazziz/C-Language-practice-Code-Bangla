addf4e0939ad22ba367f8959915f38c927d95a34
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-03-04 01:24:40 +0600

    C program input for adding two numbers

--- 11. C Language Algorithm, flowchart  C program for adding two numbers/main.c
+++ 11. C Language Algorithm, flowchart  C program for adding two numbers/main.c
@@ -13,4 +13,25 @@ int main() {
     // printf("Addition of %d and %d = %d", num1, num2, addition);
     printf("Addition of %d", addition);
     return 0;
+
+}
+
+
+#include <stdio.h>
+
+int main()
+{
+   int num1, num2, addition;
+
+   printf("Enter your First Number: ");
+   scanf("%d", &num1);
+
+   printf("Enter your second Number: ");
+   scanf("%d", &num2);
+
+   addition = num1 + num2;
+
+   printf("\nResult: %d + %d = %d\n", num1, num2, addition);
+
+   return 0;
 }
