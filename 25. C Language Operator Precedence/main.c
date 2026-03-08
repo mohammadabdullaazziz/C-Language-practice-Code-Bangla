1395365563ec8b3e35d7c00fcfe1843311a91e36
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-03-08 22:08:57 +0600

    C Language Operator Precedence

---
+++ 25. C Language Operator Precedence/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("The world best book is: \"Al-Kuran\"\n");
+    return 0;
+}
+
+C ল্যাঙ্গুয়েজে Operator Precedence বা অপারেটরের অগ্রাধিকার বলতে বোঝায়,
+যখন একটি লাইনে অনেকগুলো অপারেটর থাকে, তখন কোন কাজটি আগে হবে। একে অনেকটা অংকের BODMAS নিয়মের সাথে তুলনা করা যায়।
