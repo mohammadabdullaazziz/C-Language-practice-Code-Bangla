542d45990f102730df114ba2acb2ea7df0c2cbf9
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-04-03 20:56:54 +0600

    C Language Increment & Decrement Operators

---
+++ 27. C Language Increment & Decrement Operators/main.c
@@ -0,0 +1,6 @@
+সি প্রোগ্রামিং ভাষায় Increment(++) এবং Decrement(--) অপারেটরগুলো হলো Unary Operators, যা ভ্যারিয়েবলের মান ১ বৃদ্ধি(increase) বা ১ হ্রাস(decrease) করতে ব্যবহৃত হয়।
+#include <stdio.h>
+    int main() {
+    
+    return 0;
+}
