8841b4025be40ca46f4a036a28e7c0d9da815004
Abdulla Bin Aziz <programmeraziz216@gmail.com>
2026-05-11 20:06:23 +0600

    C Language Logical AND operator

---
+++ 32. C Language Logical AND operator/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+  char name[] = "Mohammad";
+  int age = 28;
+  float height = 5.2f, cgpa = 3.50f;
+  char address[] = "Gonggachora, Rangpur";
+  char subject[] = "Backend Engineer";
+  bool married = false;
+  char bloodGroup[] = "B+";
+  char section = 'A';
+  char grade[] = "A-";
+  double accountBalance = 89.5982145897;
+  char gmail[] = "programmeraziz216@gmail.com";
+  char phone[] = "01568451112";
+  char country[] = "Bangladesh";
+  char dob[] = "15-06-1997";
+  char religion[] = "Islam";
+  char skills[] = "C, JavaScript, C++, Python";
+  char hobbies[] = "Coding, Reading, Traveling";
+  char session[] = "2025-26";
+
+  printf("-----------Student Information-----------\n");
+  printf("")
+
+  return 0;
+}   
